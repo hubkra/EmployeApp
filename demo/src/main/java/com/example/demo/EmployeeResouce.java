7e0d4526bb6cf4f90634e9f66ae5a29951aa422e
Hubert Kras <mrkrashubert@gmail.com>
2022-05-16 21:55:34 +0200

    Add Mapping to two things
    I will add more in tomorow

---
+++ demo/src/main/java/com/example/demo/EmployeeResouce.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResouce {
+    private final EmployeeService employeeService;
+
+    public EmployeeResouce(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees ()
+    {
+        List<Employee> employees = employeeService.findEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id)
+    {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+}
--- demo/src/main/java/com/example/demo/service/EmployeeService.java
+++ demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
