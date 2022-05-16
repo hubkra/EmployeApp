da928d869d9a7c0847e6dbcab947bbbeeb47ddfd
Hubert Kras <mrkrashubert@gmail.com>
2022-05-16 21:16:08 +0200

    Add service and methods

---
+++ demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public List<Employee> findEmployees()
+    {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee)
+    {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id)
+    {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("Uzytkownik o id "+id + " nie został odnaleziony"));
+    }
+    public void deleteEmployee(Long id)
+    {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
