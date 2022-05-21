a1261b0548c47ffb5fd6b928d8064bf06d20a39a
Hubert Kras <mrkrashubert@gmail.com>
2022-05-21 17:14:20 +0200

    vue in other folder

---
+++ front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+  <MyEmployee />
+  </div>
+ 
+  
+</template>
+
+<script>
+import MyEmployee from './components/MyEmployee.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    MyEmployee
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/MyEmployee.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <div class="container">
+        <h1 class="text-center"> Employee List</h1>
+        <table class="table">
+            <thead>
+                <th>Employee ID </th>
+                <th>Name </th>
+                <th>email </th>
+                <th>JobTitle</th>
+                <th>image </th>
+            </thead>
+            <tbody>
+                <tr v-for = "employee in employee" v-bind:key="employee.id">
+                    <td>{{employee.id}}</td>
+                    <td>{{employee.name}}</td>
+                    <td>{{employee.email}}</td>
+                    <td>{{employee.jobTitle}}</td>
+                    <td>{{employee.imageUrl}}</td>
+                    
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+
+</template>
+
+<script>
+import EmployeeSevice from '@/services/EmployeeSevice'
+export default {
+    name: 'FirmEmployee',
+    data(){
+        return{
+        employee : []
+        }
+    },
+    methods: {
+        getEmployees(){
+            EmployeeSevice.getEmployees().then((response)=> {
+                this.employee = response.data;
+            });
+        }
+
+    },
+    created(){
+        this.getEmployees()
+    }
+}
+
+
+</script>
+
+
+<style>
+
+</style>
