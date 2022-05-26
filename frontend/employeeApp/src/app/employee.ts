1173b54e67301d714772e15f86cd6399b0e8eb3e
Hubert Kras <mrkrashubert@gmail.com>
2022-05-26 19:37:18 +0200

    Create Employee Component and Generate Service

--- frontend/employeeApp/src/app/employee.ts
+++
@@ -1,7 +0,0 @@
-export interface Employee {
-    id:number;
-    name:string;
-    email:string;
-    jobTitle:string;
-    imageUrl:string;
-}
--- frontend/employeemanagerapp/src/app/app.module.ts
+++ frontend/employeemanagerapp/src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [EmployeeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
